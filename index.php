a43abee41a9570dd249d2d11b9cac9d8e335fe0c
Rowin <rowinvgils@gmail.com>
2023-09-27 11:17:55 +0200

    stukje tekst

--- index.php
+++ index.php
@@ -1,32 +1,19 @@
 <?php
-/**
- * Server variabelen
- * SERVER
- * GET
- * POST
- */
 
+$routes = [
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/details" => "controllers/details.php",
+    "/contact" => "controllers/contact.php"
+    
+];
 
+if(array_key_exists($_SERVER['REQUEST_URI'], $routes)){
+    require $routes[$_SERVER['REQUEST_URI']];
+} 
 
-/**
- * Routing
- * Waar in de applicatie ben je?
- *
- * Voor het gebruik gaan we er gebruik van maken van de volgende opbouw
- * - index(home)
- * - contact
- * - details
- *      - profiel
- *      - cijfers
- *      - ervaringen
- *      - hobby
- * - about
- */
+else {
+    echo "error code:404";
+}
 
-
-
-
-/**
- * PDO - connect to database
- *
- */
+?>
